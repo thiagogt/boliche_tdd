43700107a43abbcb2538be052436e5027e2bdb72
THIAGO GOMES TOLEDO <ttoledo.everify@digio.com.br>
2022-10-11 00:34:02 -0300

    break test with special caracters

--- src/test/java/com/cursoteste/boliche_tdd/pontuacao/PontuacaoTest.java
+++ src/test/java/com/cursoteste/boliche_tdd/pontuacao/PontuacaoTest.java
@@ -14,8 +14,8 @@ public class PontuacaoTest {
     @Test
     public void Deve_RetornarAPontuacaoTotal_Quando_QuadrosValidosForemDados()
     {
-        String[] rodadaInicial = new String[]{"8","0"};
-        String[] rodadaFinal = new String[]{"8","1","0"};
+        String[] rodadaInicial = new String[]{"8","/"};
+        String[] rodadaFinal = new String[]{"X","-","1"};
 
 
         Pontuacao pontuacao = new Pontuacao()
